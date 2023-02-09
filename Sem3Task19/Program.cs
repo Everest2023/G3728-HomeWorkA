6af0f14039fe01a64535932fd95922ef099ff9c2
nadezhda <abuziarova.nadezhda@gmail.com>
2023-02-09 17:21:39 +0300

    ДЗ 19

--- Sem3Task19/Program.cs
+++ Sem3Task19/Program.cs
@@ -5,54 +5,56 @@
 // 23432 -> да
 //* Сделать вариант через СЛОВАРЬ четырехзначных палиндромов
 
-Console.WriteLine("Введите пятизначное число:");
-char[] digit = Console.ReadLine().ToCharArray();
+// Console.WriteLine("Введите пятизначное число:");
+// char[] digit = Console.ReadLine().ToCharArray();
 
-// считается длинна введенного числа в массиве если 
-if (digit.Length > 5)
-{
-    Console.WriteLine("не верно, введите 5 значьное число"); 
-}
+// // считается длинна введенного числа в массиве если 
+// if (digit.Length > 5)
+// {
+//     Console.WriteLine("не верно, введите 5 значьное число"); 
+// }
 
-else
-    if ((digit[0]==digit[4]) && (digit[1]==digit[3]))
-  {
-    Console.WriteLine("является полиндромом");
-  }
+// else
+//     if ((digit[0]==digit[4]) && (digit[1]==digit[3]))
+//   {
+//     Console.WriteLine("является полиндромом");
+//   }
     
-  else
-  {
-    Console.WriteLine("не является полиндромом");
-  }
+//   else
+//   {
+//     Console.WriteLine("не является полиндромом");
+//   }
 
 // //Вариант 2
-//  int num = ReadData("Введите пятизначное число:");
+
  
-// // //Метод читает данные от пользователя
-//  int ReadData(string msg) 
-// { //метод считывающий целое число из консоли, выводя в неё сообщение
-//     Console.Write(msg);
-//     return int.Parse(Console.ReadLine() ?? "0");
-// }
+//Метод читает данные от пользователя
+int ReadData(string msg) 
+{
+//метод считывающий целое число из консоли, выводя в неё сообщение
+Console.Write(msg);
+return int.Parse(Console.ReadLine() ?? "0");
+}
  
-// //Выводим результат пользователю
-// void PrintData(string msg, bool result)
-// {
-//      Console.WriteLine(msg + result);
-// }
+//Выводим результат пользователю
+void PrintData(string msg, bool result)
+ {
+     Console.WriteLine(msg + result);
+ }
 
 
-// bool PalinTest(int num)
-// {
-//     bool result = false;
-//     int d1 = num/10000;
-//     int d2 = (num/1000)% 10;
-//     int d3 = (num/10)%10;
-//     int d4 = num%10;
-//     result = ((d1==d4) && (d2==d3))? true:false; 
-//     return result;
-// }
+ bool PalinTest(int num)
+{
+     bool result = false;
+     int d1 = num/10000;
+     int d2 = (num/1000)% 10;
+     int d3 = (num/10)%10;
+     int d4 = num%10;
+     result = ((d1==d4) && (d2==d3))? true:false; 
+     return result;
+}
 
+int num = ReadData("Введите пятизначное число:");
 
-
-// PrintData("Расстояние между точками: ",  result);
+bool result = PalinTest(num);
+PrintData("Расстояние между точками: ",  result);
